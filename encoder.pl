09b40cdd16ae3657fa90eef283b950b5a02d1f9c
dbk3r <bk.pc@gmx.de>
2016-01-14 22:14:05 +0100

    add functions.pl

--- encoder.pl
+++ encoder.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/perl -w
 
 use Config::Simple;
 use Config;
@@ -12,7 +13,7 @@ use IPC::Run qw(start pump);
 
 $os = $Config{osname};
 $SIG{INT} = \&interrupt;
-$host = hostname();
+#$host = hostname();
 
 if ($os eq "MSWin32")
 {
@@ -110,51 +111,30 @@ sub runloop {
 			
 		}
 	}	
+	#$dbh->disconnect();
 }
 
 sub render_job {
 	
-	if ($workflow eq "blender")
+	if ($content_type eq "blender")
 	{
 		#its a blender file
-		
-		@cmd = ($blender_bin, "-b", $sourcefile, "-o" , $output_folder , "-F" , $output_format , "-s" , $startframe , "-e" , $endframe , "-a");
-		
-		
-		$harness = start \@cmd, \$in, \$out, \$out;		
-		$in = '';
-		
-		pump $harness while length $in;
-		
-		while(1){
-			pump $harness until $out =~ s/(.+)\n$//gm ;
-			#pump $harness until $out =~ /^\s*Fra/;
-			$full_line = $1;
-			$remaining = $full_line;
-			if ($remaining =~ /^\s*Fra/)
-			{
-				if (index($remaining, "Path Tracing Tile") > 1) 
-				{
-					print substr($remaining,index($remaining, "Path Tracing Tile") + 18,7) . "\r";
-				}
-			}				
-			
-			last if ("Blender quit" eq $full_line);
-			
-		}
-		
+		$cmd = $blender_bin ." -b " .  $content_dir . $uuid . "/" . $job_filename . " " . $job_cmd;
+		render($job_id, $cmd);	
 	}	
-	elsif ($workflow eq "video_from_sequence")
+
+	elsif ($content_type eq "sequence")
 	{			
 		$start_number = "-start_number ". $startframe;
 		$input_sequence = $sourcefile . "/%04d.png";
 		$cmd = $ffmpeg_bin . " -r 25 -i ". $input_sequence . " ". $start_number. " -vcodec libx264 -b:v 4000k  ". $output_folder . "/" . $scene_name .".mp4";
 		system($cmd);		
 	}
-	elsif ($workflow eq "transcode")
+
+	elsif ($content_type eq "video" || $content_type eq "audio")
 	{
-		$cmd = $ffmpeg_bin . " -i ". $sourcefile . " -vcodec libx264 -b:v 4000k  ". $output_folder . "/" . $scene_name .".mp4";
-		system($cmd);
+		$cmd = $ffmpeg_bin . " " .  $job_cmd . " " . $content_dir.$job_uuid."/" . $job_filename;
+		transcode($job_id,$cmd);
 	}
 		
 }
@@ -167,19 +147,18 @@ sub read_jobs_db {
 	if ($jobcount > 0)
 	{
 		while (my $jobrow = $jobresult->fetchrow_hashref) {
-			$job_id= $jobrow->{id};
-			$sourcefile= $jobrow->{sourcefile};
-			$output_folder= $jobrow->{output_folder};
-			$output_format= $jobrow->{output_format};
-			$startframe= $jobrow->{startframe};
-			$endframe= $jobrow->{endframe};
-			$project_name= $jobrow->{project_name};
-			$scene_name= $jobrow->{scene_name};	
-			$workflow = $jobrow->{workflow};
+			$job_id = $jobrow->{id};
+			$job_cmd = $jobrow->{cmd};	
+			$content_type = $jobrow->{content_type};	
+			$job_prio = $jobrow->{prio};
+			$job_filename = $jobrow->{filename};
+			$job_uuid = $jobrow->{uuid};
 		}
 	}
 	
 }
+
+
 
 sub read_encoder_db {
 
